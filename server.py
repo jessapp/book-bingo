95c34428f702d8bf526b29f329ea2438ac754da4
Jessica Appelbaum <jess.appelbaum@gmail.com>
2017-02-09 00:01:43 +0000

    Fixed board display on user page

--- server.py
+++ server.py
@@ -125,8 +125,7 @@ def display_user(user_id):
     return render_template("user_details.html",
                     first_name=first_name,
                     board_id=board_id,
-                    boards=boards,
-                    board_name=board_name)
+                    boards=boards)
 
 
 @app.route('/create-board', methods=["GET"])
@@ -165,12 +164,14 @@ def create_board():
         if i == 12:
             genres_text.append("FREE SQUARE")
         genres_text.append(request.form.get("genre" + str(i+1)))
+
     
     # Instantiate genre objects
     genre_objects = []
 
     for genre_text in genres_text:
         genre_objects.append(Genre(name=genre_text))
+
 
     # Create squares
     squares = []
@@ -191,145 +192,9 @@ def create_board():
         square.genre = genre_object
         new_board.squares.append(square) 
     
-    # genre1_name = request.form.get("genre1")
-    # genre2_name = request.form.get("genre2")
-    # genre3_name = request.form.get("genre3")
-    # genre4_name = request.form.get("genre4")
-    # genre5_name = request.form.get("genre5")
-    # genre6_name = request.form.get("genre6")
-    # genre7_name = request.form.get("genre7")
-    # genre8_name = request.form.get("genre8")
-    # genre9_name = request.form.get("genre9")
-    # genre10_name = request.form.get("genre10")
-    # genre11_name = request.form.get("genre11")
-    # genre12_name = request.form.get("genre12")
-    # genre13_name = "FREE SQUARE"
-    # genre14_name = request.form.get("genre13")
-    # genre15_name = request.form.get("genre14")
-    # genre16_name = request.form.get("genre15")
-    # genre17_name = request.form.get("genre16")
-    # genre18_name = request.form.get("genre17")
-    # genre19_name = request.form.get("genre18")
-    # genre20_name = request.form.get("genre19")
-    # genre21_name = request.form.get("genre20")
-    # genre22_name = request.form.get("genre21")
-    # genre23_name = request.form.get("genre22")
-    # genre24_name = request.form.get("genre23")
-    # genre25_name = request.form.get("genre24")
-
-    # genre1 = Genre(name=genre1_name)
-    # genre2 = Genre(name=genre2_name)
-    # genre3 = Genre(name=genre3_name)
-    # genre4 = Genre(name=genre4_name)
-    # genre5 = Genre(name=genre5_name)
-    # genre6 = Genre(name=genre6_name)
-    # genre7 = Genre(name=genre7_name)
-    # genre8 = Genre(name=genre8_name)
-    # genre9 = Genre(name=genre9_name)
-    # genre10 = Genre(name=genre10_name)
-    # genre11 = Genre(name=genre11_name)
-    # genre12 = Genre(name=genre12_name)
-    # genre13 = Genre(name=genre13_name)
-    # genre14 = Genre(name=genre14_name)
-    # genre15 = Genre(name=genre15_name)
-    # genre16 = Genre(name=genre16_name)
-    # genre17 = Genre(name=genre17_name)
-    # genre18 = Genre(name=genre18_name)
-    # genre19 = Genre(name=genre19_name)
-    # genre20 = Genre(name=genre20_name)
-    # genre21 = Genre(name=genre21_name)
-    # genre22 = Genre(name=genre22_name)
-    # genre23 = Genre(name=genre23_name)
-    # genre24 = Genre(name=genre24_name)
-    # genre25 = Genre(name=genre25_name)
-
-    # Create squares
-
-    # square1 = Square(x_coord=1, y_coord=1)
-    # square2 = Square(x_coord=1, y_coord=2)
-    # square3 = Square(x_coord=1, y_coord=3)
-    # square4 = Square(x_coord=1, y_coord=4)
-    # square5 = Square(x_coord=1, y_coord=5)
-    # square6 = Square(x_coord=2, y_coord=1)
-    # square7 = Square(x_coord=2, y_coord=2)
-    # square8 = Square(x_coord=2, y_coord=3)
-    # square9 = Square(x_coord=2, y_coord=4)
-    # square10 = Square(x_coord=2, y_coord=5)
-    # square11 = Square(x_coord=3, y_coord=1)
-    # square12 = Square(x_coord=3, y_coord=2)
-    # square13 = Square(x_coord=3, y_coord=3)
-    # square14 = Square(x_coord=3, y_coord=4)
-    # square15 = Square(x_coord=3, y_coord=5)
-    # square16 = Square(x_coord=4, y_coord=1)
-    # square17 = Square(x_coord=4, y_coord=2)
-    # square18 = Square(x_coord=4, y_coord=3)
-    # square19 = Square(x_coord=4, y_coord=4)
-    # square20 = Square(x_coord=4, y_coord=5)
-    # square21 = Square(x_coord=5, y_coord=1)
-    # square22 = Square(x_coord=5, y_coord=2)
-    # square23 = Square(x_coord=5, y_coord=3)
-    # square24 = Square(x_coord=5, y_coord=4)
-    # square25 = Square(x_coord=5, y_coord=5)
-    
-    #Append squares to board and assign genres
-
-    # new_board.squares.append(square1)
-    # square1.genre = genre1
-    # new_board.squares.append(square2)
-    # square2.genre = genre2
-    # new_board.squares.append(square3)
-    # square3.genre = genre3
-    # new_board.squares.append(square3)
-    # square3.genre = genre3
-    # new_board.squares.append(square4)
-    # square4.genre = genre4
-    # new_board.squares.append(square5)
-    # square5.genre = genre5
-    # new_board.squares.append(square5)
-    # square5.genre = genre5
-    # new_board.squares.append(square6)
-    # square6.genre = genre6
-    # new_board.squares.append(square7)
-    # square7.genre = genre7
-    # new_board.squares.append(square8)
-    # square8.genre = genre8
-    # new_board.squares.append(square9)
-    # square9.genre = genre9
-    # new_board.squares.append(square10)
-    # square10.genre = genre10
-    # new_board.squares.append(square11)
-    # square11.genre = genre11
-    # new_board.squares.append(square12)
-    # square12.genre = genre12
-    # new_board.squares.append(square13)
-    # square13.genre = genre13
-    # new_board.squares.append(square14)
-    # square14.genre = genre14
-    # new_board.squares.append(square15)
-    # square15.genre = genre15
-    # new_board.squares.append(square16)
-    # square16.genre = genre16
-    # new_board.squares.append(square17)
-    # square17.genre = genre17
-    # new_board.squares.append(square18)
-    # square18.genre = genre18
-    # new_board.squares.append(square19)
-    # square19.genre = genre19
-    # new_board.squares.append(square20)
-    # square20.genre = genre20
-    # new_board.squares.append(square21)
-    # square21.genre = genre21
-    # new_board.squares.append(square22)
-    # square22.genre = genre22
-    # new_board.squares.append(square23)
-    # square23.genre = genre23
-    # new_board.squares.append(square24)
-    # square24.genre = genre24
-    # new_board.squares.append(square25)
-    # square25.genre = genre25
-    
     db.session.add(new_board)
     db.session.commit()
+
 
     # get new board ID from DB
     board_id = new_board.board_id
